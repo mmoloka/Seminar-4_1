19979c30c50360b999b128545c1c9b9184ddc859
«Mikhail» <kopfgolova@gmail.com>
2022-04-28 15:30:52 +0300

    add Task 7

---
+++ Task 7/Program.cs
@@ -0,0 +1,10 @@
+﻿/*
+ Напишите программу, которая на вход принимает значение, а
+на выходе показывает обратное значение.
+1 -> 1
+2 -> 0.5
+0.25 -> 4
+*/
+Console.WriteLine("Введите число ");
+double number = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine(1 / number);
